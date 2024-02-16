fabc90e3feb5e426f3ca910487c17188238b5e0b
hariprasafdreddy <hariprasad1999@gmail.com>
2024-02-16 09:32:15 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Welcome to NumberSystem");
+Console.WriteLine("***Even Nums***");
+NumberSystem.EvenNum();
+Console.WriteLine("***Odd Nums***");
+NumberSystem.OddNum();
